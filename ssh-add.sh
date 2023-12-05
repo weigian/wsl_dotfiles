d62cdb7a9ef261f32dceda3857a495fa6efcfe73
walterc.wsl <walter886@gmail.com>
2023-12-05 20:07:02 +0800

    fix the key fail root cause

--- ssh-add.sh
+++ ssh-add.sh
@@ -23,3 +23,12 @@ fi
 
 ssh -T git@github.com
 
+#  remote: Please see https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls for information on currently recommended modes of authentication.
+# walterc@NB:~/wsl_dotfiles$ git remote -v
+#  origin  https://github.com/weigian/wsl_dotfiles (fetch)
+#  origin  https://github.com/weigian/wsl_dotfiles (push)
+# walterc@NB:~/wsl_dotfiles$ git remote set-url origin git@github.com:weigian/wsl_dotfiles
+# walterc@NB:~/wsl_dotfiles$ git remote -v
+#  origin  git@github.com:weigian/wsl_dotfiles (fetch)
+#  origin  git@github.com:weigian/wsl_dotfiles (push)
+# walterc@NB:~/wsl_dotfiles$ git push
